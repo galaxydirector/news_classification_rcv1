3d2e25f9a49e958bbbdb21cb90436ea56948939a
galaxydirector <zhangy8665@gmail.com>
2018-10-30 14:51:43 -0400

    update tf version of algo

--- optimizers.py
+++ optimizers.py
@@ -32,9 +32,56 @@ def PEGASOS(x,y,T,lamb):
 
 	return w
 
+
 def adagrad(w, prediction, target, lr):
 	"""
 	lr = 1/sqrt(T)
 	"""
 
 	return
+
+
+
+
+
+
+def PEGASOS_tf(x,y,T,lamb): #################################################### developing
+	# tensorflow version of PEGASOS algorithm
+
+
+	# initialize weights with normal of std = 0.01, mean = 0
+	w = tf.variables(tf.random_normal([tf.shape(x)[1],1],stddev=0.01)/tf.sqrt(lamb), name='weights')
+
+	row_per_update = tf.floor_div(tf.shape(x)[0], T)
+
+	sub_x = tf.placeholder(tf.float32, shape=[row_per_update, tf.shape(x)[1]])
+	sub_y = tf.placeholder(tf.float32, shape=[row_per_update, tf.shape(y)[1]])
+	ite = tf.placeholder(tf.int32, shape=(1))
+
+
+	output = tf.multiply(tf.matmul(sub_x,w),sub_y)
+	output_ind = tf.greater(output,0) 
+	gradient = tf.multiply(lamb,w) - tf.divide(tf.reduce_sum(\
+				tf.multiply(tf.boolean_mask(sub_y,output_ind),tf.boolean_mask(sub_x,output_ind)), axis=0),row_per_update) 
+
+	lr = 1/(ite*lamb)
+
+	raw_w = w - lr*gradient
+	update_w = w.assign(tf.minimum(1,1/tf.sqrt(lamb)/tf.norm(raw_w))*raw_w)
+
+	# tf.sqrt(sum([i**2 for i in raw_w])) # is norm?
+
+	init = tf.global_variables_initializer()
+	with tf.Session() as sess:
+	    sess.run(init)
+
+		for i in range(T):
+			new_w = sess.run(update_w, feed_dict = {sub_x: x[i:(i+row_per_update),:],\
+													sub_y: y[i:(i+row_per_update),:],\
+													ite:   i})
+
+	return new_w
+
+
+# # initialize variables
+# x = tf.variables()	
